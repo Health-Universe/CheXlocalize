558e33a724fbc6863e2d2cf1b3c0bed00393f99d
Kinal Patel <136503460+kinal1999@users.noreply.github.com>
2023-07-07 14:14:44 -0500

    Update 3_Generate_Segmentations.py

--- pages/3_Generate_Segmentations.py
+++ pages/3_Generate_Segmentations.py
@@ -17,126 +17,118 @@ from PIL import Image, ImageDraw
 from pycocotools import mask
 import time
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "CheXlocalize"))
-
-from eval_constants import CHEXPERT_TASKS, LOCALIZATION_TASKS
-from utils import CPU_Unpickler, encode_segmentation, parse_pkl_filename
-from heatmap_to_segmentation import cam_to_segmentation, pkl_to_mask, heatmap_to_mask
-
-st.markdown("## Generate Segmentations")
+st.markdown("## Generate Segmnetations")
 st.divider()
 
 st.subheader("Saliency Heatmap Segmentations")
+st.write("Coming Soon!")
 
-def process_files(pkl_files):
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+st.divider()
+st.subheader("Human Annotation Segmentations")
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "CheXlocalize"))
+
+from eval_constants import LOCALIZATION_TASKS
+from utils import encode_segmentation
+from annotation_to_segmentation import create_mask, ann_to_mask
+
+def convert_annotations_to_segmentations(annotations):
     segmentations = {}
-    for pkl_file in pkl_files:
-        pkl_data = pkl_file.read()
+    for img_id, img_data in annotations.items():
+        if img_id not in segmentations:
+            segmentations[img_id] = {}
+        for task, polygons in img_data.items():
+            img_dims = img_data['img_size']
+            try:
+                segm_map = create_mask(polygons, img_dims)
+                encoded_map = encode_segmentation(segm_map)
+                segmentations[img_id][task] = encoded_map
+            except TypeError:
+                segmentations[img_id][task] = None
+    return segmentations
 
-        # Create a temp file
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            temp_file.write(pkl_data)
-            temp_file_path = temp_file.name
-
-        try:
-            # Default values
-            threshold = np.nan
-            probability_cutoff = 0
-            smoothing = False
-            kernel_size = 0
-
-            segmentation = pkl_to_mask(temp_file_path, threshold, probability_cutoff, smoothing, kernel_size)
-            encoded_segmentation = encode_segmentation(segmentation)
-            pkl_filename = pkl_file.name
-            segmentations[pkl_filename] = encoded_segmentation
-
-        except Exception as e:
-            print(f"Error occurred during segmentation: {e}")
-
-        # Remove temp file
-        Path(temp_file_path).unlink()
-
-    # Convert dictionary to JSON
-    segmentations_json = json.dumps(segmentations)
-
-    # Save JSON data to file
-    output_file = 'segmentation_data.json'
-    output_path = os.path.join(os.getcwd(), output_file)
+def save_segmentations(segmentations, output_path):
+    with open(output_path, "w") as outfile:
+        json.dump(segmentations, outfile)
+        
+def download_json(output_json, output_path):
     with open(output_path, 'w') as f:
-        f.write(segmentations_json)
-
-    return output_path
-
+        f.write(output_json)
+    with open(output_path, 'r') as f:
+        data = f.read()
+    st.markdown("---")
+    st.markdown("### Download")
+    st.download_button(
+        label="Download Generated Segmentations",
+        data=data,
+        file_name=output_path,
+        mime="application/json"
+    )
 
 # File uploader
-pkl_files = st.file_uploader('Upload', type='pkl', accept_multiple_files=True, help="**Input:** Pickle files containing heatmaps (see Example Input).\n\n**Output:** JSON of binary segmentations.")
-
-# Optional Paths
-with st.expander("Optional Paths"):
-    st.write ("Coming Soon!")
+uploaded_file = st.file_uploader('Upload', type='json', help="**Input:** JSON Human annotations file (see Example Input).\n\n**Output:** JSON of segmentations.")
 
 # Example Input
 with st.expander("Example Input"):
     st.code("""
 {
-# DenseNet121 + Grad-CAM heatmap <torch.Tensor> of shape (1, 1, h, w)
-'map': tensor([[[[1.4711e-06, 1.4711e-06, 1.4711e-06,  ..., 5.7636e-06, 5.7636e-06, 5.7636e-06],
-                [1.4711e-06, 1.4711e-06, 1.4711e-06,  ..., 5.7636e-06, 5.7636e-06, 5.7636e-06],
-                ...,
-             [0.0000e+00, 0.0000e+00, 0.0000e+00,  ..., 7.9709e-05, 7.9709e-05, 7.9709e-05],
-                [0.0000e+00, 0.0000e+00, 0.0000e+00,  ..., 7.9709e-05, 7.9709e-05, 7.9709e-05]]]]),
-
-# model probability (float)
-'prob': 0.02029409697279334,
-
-# one of the ten possible pathologies (string)
-'task': Consolidation,
-
-# 0 if ground-truth label for 'task' is negative, 1 if positive (int)
-'gt': 0,
-
-# original cxr image
-'cxr_img': tensor([[[0.7490, 0.7412, 0.7490,  ..., 0.8196, 0.8196, 0.8118],
-              [0.6627, 0.6627, 0.6706,  ..., 0.7373, 0.7137, 0.6941],
-                  [0.5137, 0.5176, 0.5294,  ..., 0.6000, 0.5686, 0.5255],
-                  ...,
-                  [0.7294, 0.7725, 0.7804,  ..., 0.2941, 0.2549, 0.2078],
-                  [0.7804, 0.8157, 0.8157,  ..., 0.3216, 0.2824, 0.2510],
-                  [0.8353, 0.8431, 0.8549,  ..., 0.3725, 0.3412, 0.3137]],
-                  ...
-                [[0.7490, 0.7412, 0.7490,  ..., 0.8196, 0.8196, 0.8118],
-                  [0.6627, 0.6627, 0.6706,  ..., 0.7373, 0.7137, 0.6941],
-                  [0.5137, 0.5176, 0.5294,  ..., 0.6000, 0.5686, 0.5255],
-                  ...,
-                  [0.7294, 0.7725, 0.7804,  ..., 0.2941, 0.2549, 0.2078],
-                  [0.7804, 0.8157, 0.8157,  ..., 0.3216, 0.2824, 0.2510],
-                  [0.8353, 0.8431, 0.8549,  ..., 0.3725, 0.3412, 0.3137]]]),
-
-# dimensions of original cxr (w, h)
-'cxr_dims': (2022, 1751)
+    'patient64622_study1_view1_frontal': {
+        'img_size': [2320, 2828], # (h, w)
+        'Support Devices': [[[1310.68749, 194.47059],
+                            [1300.45214, 194.47059],
+                            [1290.21691, 201.29412],
+                            ...
+                            [1310.68749, 191.05883],
+                            [1300.45214, 197.88236],
+                            [1293.62865, 211.52943]]],
+     'Cardiomegaly': [[[1031.58047, 951.35314],
+                 [1023.92373, 957.09569],
+                 [1012.43856, 964.75249],
+              ...
+              [1818.31313, 960.92406],
+                 [1804.91384, 955.1815],
+                 [1789.60024, 951.35314]]],
+    ...
+    },
+    'patient64542_study1_view2_lateral': {
+        ...
+    }
 }
     """, language="python")
+
+# File Processing
+if uploaded_file is not None:
+    run_button = st.button('Run', key='segmentation_run_button', help="Generating Segmentations")
     
-# Process Files
-run_button = st.button('Run', help="Generating Segmentations")
+    if run_button:
+        with st.spinner('Running...'):
+            annotations = json.load(uploaded_file)
+            segmentations = convert_annotations_to_segmentations(annotations)
+            st.session_state["segmentations"] = segmentations
 
-# Process uploaded files
-if pkl_files and run_button:
-    with st.spinner("Running"):
-        segmentations_output = process_files(pkl_files)
-    st.success("Segmentations generated successfully.")
+# Download button
+if "segmentations" in st.session_state:
+    segmentations = st.session_state["segmentations"]
+    with tempfile.NamedTemporaryFile(suffix=".json", delete=False) as temp_output_file:
+        temp_output_path = temp_output_file.name
+        save_segmentations(segmentations, temp_output_path)
 
-    # Download button for outputted JSON file
-    with open(segmentations_output, 'r') as f:
-        data = f.read()
-    st.download_button(
-        label="Download Generated Segmentations",
-        data=data,
-        file_name="saliency_segmentations.json",
-        mime="application/json"
-    )
-
-st.divider()
-
-st.subheader("Human Annotation Segmentations")
-st.write("Coming Soon!")
+    # Read JSON file
+    with open(temp_output_path, 'r') as f:
+        output_json = f.read()
+    
+    # Download button for output
+    download_json(output_json, temp_output_path)
